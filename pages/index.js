93b20881cf7099f2c9c50a9ae24e0d9ec7d8d4cc
FrankBeatrice <frankiebeatriceiii@gmail.com>
2021-07-26 16:43:19 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,9 @@ export default function Home() {
     loadNFTs()
   }, [])
   async function loadNFTs() {    
-    const provider = new ethers.providers.JsonRpcProvider()
+   /* Upadted this: const provider = new ethers.providers.JsonRpcProvider() */
+/* To ->  */  
+    const provider = new ethers.providers.JsonRpcProvider("https://8545-youendpoint.gitpod.io/")
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
     const data = await marketContract.fetchMarketItems()
